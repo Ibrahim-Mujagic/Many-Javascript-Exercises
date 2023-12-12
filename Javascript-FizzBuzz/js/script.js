95f02d0f77e1b8e7b8d89abaf39a15691f0e1269
Ibrahim-Mujagic <mujagicibrahim1@gmail.com>
2023-12-12 18:44:59 +0100

    Added fizzbuzz

--- Javascript-FizzBuzz/js/script.js
+++ Javascript-FizzBuzz/js/script.js
@@ -1,1 +1,27 @@
-console.log('hello');
+insertingBoxes();
+
+function insertingBoxes(){
+  const contBoxes = document.querySelector('.cont-boxes');
+  
+  for (let i = 0; i < 100; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.innerHTML = i + 1;
+
+    contBoxes.append(box)
+    checkFizzBuzz(i,box);
+  }
+}
+
+function checkFizzBuzz(i,box){
+  if (!(i % 15)) {
+    box.innerHTML = "FizzBuzz"
+    box.classList.add('fizz-buzz')
+  }else if (!(i % 3)) {
+    box.innerHTML = "Fizz"
+    box.classList.add('fizz')
+  }else if (!(i % 5)) {
+    box.innerHTML = "Buzz"
+    box.classList.add('buzz')
+  }
+}
